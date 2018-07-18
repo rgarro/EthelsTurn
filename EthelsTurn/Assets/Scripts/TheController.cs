831b5923c83cb1ad723d6fd24c1100f4d02d7232
rgarro <rgarro@gmail.com>
2018-07-17 21:57:53 -0600

    咸水平原
    咸水平原

---
+++ EthelsTurn/Assets/Scripts/TheController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Here we go");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ EthelsTurn/Assets/Scripts/WheelData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelData {
+
+	public Transform wheelTransform; //4
+	public WheelCollider col; //5
+	public Vector3 wheelStartPos; //6 
+	public float rotation = 0.0f;  //7
+}
